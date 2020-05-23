f497cd8531d8817a5b1a4c8f2141445998af33fc
FusilliBolognese <fusillibolognese@gmail.com>
2020-05-23 21:50:36 +0200

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-test1';
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes: Hero[] = [
+    new Hero(1, "super-man"),
+    new Hero(2, "spider-man"),
+    new Hero(3, "ant-man"),
+  ];
+  favoriteHero: Hero;
+
+  ngOnInit(): void {
+    this.favoriteHero = this.heroes[0];
+  }
+
 }
